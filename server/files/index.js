2e8c98b06db8e3f6dd786428925aa91b3653dcd6
Tobias Schmidt <tobias.ws.schmidt@outlook.com>
2026-04-16 22:03:54 +0200

    feat/added edit button

--- server/files/index.js
+++ server/files/index.js
@@ -25,10 +25,28 @@ window.onload = function () {
 
                 const header = document.createElement("header");
 
+                // Create a wrapper for title + edit button
+                const titleRow = document.createElement("div");
+                titleRow.classList.add("title-row");
+
                 // Title
                 const title = document.createElement("h2");
                 title.textContent = movie.title;
-                header.append(title);
+                titleRow.append(title);
+
+                // Edit
+                const editButton = document.createElement("button");
+                editButton.id = movie.imdbID + "-edit";
+                editButton.classList.add("edit-btn");
+                editButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
+                <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
+                <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z"/>
+                </svg>`;
+                editButton.setAttribute("aria-label", `Edit ${movie.title} movie details`);
+                editButton.setAttribute("title", `Edit ${movie.title}`);
+
+                titleRow.append(editButton);
+                header.append(titleRow);
 
                 // Genres as tags
                 const genres = document.createElement("div");
